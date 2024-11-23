93d8f0ffef318ec20d43d70817f10d27d832d489
misty <yessine@gmail.com>
2024-11-23 23:43:32 +0100

    fixed someting xd

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -8,7 +8,7 @@ const AuthPage = () => {
     setIsLogin(newState) ; 
   }
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-red-500 to-pink-500 via-pink-300 '>
+    <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-red-500 to-pink-700 via-pink-300 '>
         <div className='w-full max-w-md'>
             <h2 className='text-center text-3xl font-extrabold text-white mb-8'>
                 {isLogin ? "Sign in to Swipe" : "Create a Swipe account"}
